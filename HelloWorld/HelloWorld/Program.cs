096cee5e296a3234d7f1f80016e11ada22a57379
Stephan Raab <stephan.raab@nyhart.com>
2017-01-18 15:25:21 -0500

    variables and types

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,8 +10,35 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            //string myValue = "Stephan";
+
+            //int number1 = 5;
+            //int number2 = 10;
+            //int sum = number1 + number2;
+
+            //Console.WriteLine("Hello "+ myValue);
+            //Console.WriteLine(number1 + " + " + number2 + " = " + sum);
+            //Console.ReadLine();
+
+            int num1 = 0;
+            int num2 = 0;
+            int answer = 0;
+
+            Console.WriteLine("Enter a number: ");
+            string sNum1 = Console.ReadLine();
+
+            Console.WriteLine("Enter another number: ");
+            string sNum2 = Console.ReadLine();
+
+            Int32.TryParse(sNum1, out num1);
+            Int32.TryParse(sNum2, out num2);
+
+            answer = num1 + num2;
+
+            Console.WriteLine("The sum of " + num1 + " and " + num2 + " is: " + answer);
+
             Console.ReadLine();
+
         }
     }
 }
